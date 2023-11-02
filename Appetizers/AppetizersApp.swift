cf6f48311ec091e6aa28aa74adc44ae6a17361cc
Atikur Rahman <mratikurrahman@gmail.com>
2023-11-03 03:09:00 +0600

    Project has created

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Atikur Rahman on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

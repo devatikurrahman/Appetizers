c513f47184b0044828c767f37cede53ccfb3eaf6
Atikur Rahman <mratikurrahman@gmail.com>
2023-11-05 00:39:47 +0600

    Updated Appetizer model and AppetizerListView

--- Appetizers/Appetizer.swift
+++ Appetizers/Appetizer.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Appetizer: Decodable {
+struct Appetizer: Decodable, Identifiable {
     let id: Int
     let name: String
     let description: String
@@ -23,14 +23,16 @@ struct AppetizerResponse {
 }
 
 struct MockData {
-    static let sampleAppetizer = Appetizer(id: 0001,
-                                       name: "Test Appetizer",
-                                       description: "This is the description for my appetizer. It's yummy",
-                                       price: 9.99,
-                                       imageURL: "",
-                                       calories: 99,
-                                       protein: 99,
-                                       carbs: 99)
+    static let sampleAppetizer = [
+        Appetizer(id: 0001, name: "Test Appetizer", description: "This is the description for my appetizer. It's yummy",
+                  price: 9.99, imageURL: "", calories: 99, protein: 99, carbs: 99),
+        Appetizer(id: 0002, name: "Test Appetizer", description: "This is the description for my appetizer. It's yummy",
+                  price: 9.99, imageURL: "", calories: 99, protein: 99, carbs: 99),
+        Appetizer(id: 0003, name: "Test Appetizer", description: "This is the description for my appetizer. It's yummy",
+                  price: 9.99, imageURL: "", calories: 99, protein: 99, carbs: 99),
+        Appetizer(id: 0004, name: "Test Appetizer", description: "This is the description for my appetizer. It's yummy",
+                  price: 9.99, imageURL: "", calories: 99, protein: 99, carbs: 99),
+    ]
     
-    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+    //static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
 }
--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -10,8 +10,25 @@ import SwiftUI
 struct AppetizerListView: View {
     var body: some View {
         NavigationStack {
-            Text("Appetizer")
-                .navigationTitle("Appetizers")
+            List(MockData.sampleAppetizer) { appetizer in
+                HStack {
+                    Image(systemName: "house")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 120, height: 90)
+                        .cornerRadius(8.0)
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text(appetizer.name)
+                            .font(.title2)
+                            .fontWeight(.medium)
+                        Text("$\(appetizer.price, specifier: "%.2f")")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Appetizers")
         }
     }
 }

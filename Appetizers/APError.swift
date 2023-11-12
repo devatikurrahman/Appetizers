0edea946f47eb1626b17d939bf5ccd44f52690d1
Atikur Rahman <mratikurrahman@gmail.com>
2023-11-13 02:43:48 +0600

    Created newtork manager and error swift file

---
+++ Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Atikur Rahman on 11/13/23.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}

0cdfbc5834399c2fd5e6b894a661536f8b4631d3
Atikur Rahman <mratikurrahman@gmail.com>
2023-11-15 23:58:25 +0600

    Created color extension

---
+++ Appetizers/Color+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Extension.swift
+//  Appetizers
+//
+//  Created by Atikur Rahman on 11/15/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary1 = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let brandPrimaryUI = UIColor(named: "brandPrimary")
+}

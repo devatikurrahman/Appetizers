fee4bd4166c4788d0c5002cf6de86351d897a0e3
Atikur Rahman <mratikurrahman@gmail.com>
2023-11-15 23:47:09 +0600

    Created loading view

---
+++ Appetizers/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  Appetizers
+//
+//  Created by Atikur Rahman on 11/15/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .scaleEffect(3.0)
+            
+    }
+}
+
+#Preview {
+    LoadingView()
+}

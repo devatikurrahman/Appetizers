d7fa3d72c6061f0ec7e12459853299beb79c4f5e
Atikur Rahman <mratikurrahman@gmail.com>
2023-11-30 23:54:09 +0600

    Bug fixes

--- Appetizers/Utilities/String+Extension.swift
+++ Appetizers/Utilities/String+Extension.swift
@@ -10,7 +10,7 @@ import Foundation
 extension String {
     var isValidEmail: Bool {
         let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailPredicate = NSPredicate(format: "SELF MATCH %@", emailFormat)
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailFormat)
         return emailPredicate.evaluate(with: self)
     }
     

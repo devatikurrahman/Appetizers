2fd0864804e8c950d70f6c21e0822fd8678665a9
Atikur Rahman <mratikurrahman@gmail.com>
2023-12-09 23:26:43 +0600

    List cell image code updated in async

--- Appetizers/ViewModels/AppetizerListViewModel.swift
+++ Appetizers/ViewModels/AppetizerListViewModel.swift
@@ -23,7 +23,20 @@ import SwiftUI
                 appetizers = try await NetworkManager.shared.getAppetizers()
                 isLoading = false
             } catch {
-                alertItem = AlertContext.invalidResponse
+                if let apError = error as? APError {
+                    switch apError {
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                    case .unableToComplete:
+                        alertItem = AlertContext.invalidUnableToComplete
+                    }
+                } else {
+                    alertItem = AlertContext.invalidResponse
+                }
                 isLoading = false
             }
         }
